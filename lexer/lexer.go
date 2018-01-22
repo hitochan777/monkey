b6f5167f254536e12c5b4848d3fdd67adef57a97
Otsuki Hitoshi <hitochan777@gmail.com>
2018-01-22 20:35:55 +0900

    Add double-character comparator

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -34,7 +34,12 @@ func (l *Lexer) NextToken() token.Token {
 
 	switch l.ch {
 	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
+		if l.peekChar() == '=' {
+			l.readChar()
+			tok = token.Token{Type: token.EQ, Literal: "=="}
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
 		break
 	case '+':
 		tok = newToken(token.PLUS, l.ch)
@@ -55,7 +60,12 @@ func (l *Lexer) NextToken() token.Token {
 		tok = newToken(token.GT, l.ch)
 		break
 	case '!':
-		tok = newToken(token.BANG, l.ch)
+		if l.peekChar() == '=' {
+			l.readChar()
+			tok = token.Token{Type: token.NOT_EQ, Literal: "!="}
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
 		break
 	case ';':
 		tok = newToken(token.SEMICOLON, l.ch)
@@ -129,4 +139,12 @@ func isDigit(ch byte) bool {
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
 }
