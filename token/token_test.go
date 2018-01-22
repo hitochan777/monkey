bcbf969d75351212f086356eb2789534e92b9721
Otsuki Hitoshi <hitochan777@gmail.com>
2018-01-22 21:33:06 +0900

    Add test for token

--- token/token_test.go
+++ token/token_test.go
@@ -1,1 +1,26 @@
 package token
+
+import "testing"
+
+func TestLookupIdent(t *testing.T) {
+	// test 1: returns IDENT when the input is not a keyword
+	{
+		input := "hello"
+		var expectedTokenType TokenType = IDENT
+		if tokenType := LookupIdent(input); tokenType != expectedTokenType {
+			t.Fatalf("tokentype wrong. expected=%q, got=%q", expectedTokenType, tokenType)
+		}
+
+	}
+
+	// test 2: returns FUNCTION when the input is fn
+	{
+		input := "fn"
+		var expectedTokenType TokenType = FUNCTION
+		if tokenType := LookupIdent(input); tokenType != expectedTokenType {
+			t.Fatalf("tokentype wrong. expected=%q, got=%q", expectedTokenType, tokenType)
+		}
+
+	}
+
+}
