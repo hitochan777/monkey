8160a9feb10f6838f23e326f554c061d2ca6fa8f
Otsuki Hitoshi <hitochan777@gmail.com>
2018-02-06 08:08:07 +0900

    Evaluates bang operator

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -23,8 +23,10 @@ func Eval(node ast.Node) object.Object {
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	}
-
 	return nil
 }
 
@@ -44,4 +46,26 @@ func nativeBoolToBooleanObject(input bool) *object.Boolean {
 	}
 
 	return FALSE
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorEpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperatorEpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
 }
