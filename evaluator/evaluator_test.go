3bad9fc0ebea5bb7304e8c584b41e8a272c7382b
Otsuki Hitoshi <hitochan777@gmail.com>
2018-02-07 23:37:20 +0900

    Evaluates infix (arithmetic) expressions

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -19,6 +19,17 @@ func TestLiteralExpression(t *testing.T) {
 		{"-10", -10},
 		{"true", true},
 		{"false", false},
+		{"5 + 5 + 5 + 5 - 10", 10},
+		{"2 * 2 * 2 * 2 * 2", 32},
+		{"-50 + 100 + -50", 0},
+		{"5 * 2 + 10", 20},
+		{"5 + 2 * 10", 25},
+		{"20 + 2 * -10", 0},
+		{"50 / 2 * 2 + 10", 60},
+		{"2 * (5 + 10)", 30},
+		{"3 * 3 * 3 + 10", 37},
+		{"3 * (3 * 3) + 10", 37},
+		{"(5 + 10 * 2 + 15 / 3) * 2 + -10", 50},
 	}
 
 	for _, tt := range tests {
@@ -38,13 +49,15 @@ func testEval(input string) object.Object {
 func testObject(t *testing.T, obj object.Object, expected interface{}) bool {
 	switch v := expected.(type) {
 	case int:
-	case int64:
 		return testIntegerObject(t, obj, int64(v))
+	case int64:
+		return testIntegerObject(t, obj, v)
 	case bool:
 		return testBooleanObject(t, obj, v)
 	default:
 		t.Errorf("Unexpected type %T", v)
 	}
+
 	return false
 }
 
